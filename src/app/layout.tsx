8ffc54912ad9f2c95bb566e65ea9638bdb259b37
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-07-17 14:46:41 +0300

    Add sign in page and the initial Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
-import '@/styles/globals.css'
+import { Inter } from "next/font/google";
+
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
